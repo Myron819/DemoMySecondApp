20bfb7b9a7cf7c52b4fc4bbb550416d9414fef7d
BuildTools <unconfigured@null.spigotmc.org>
2020-05-04 12:55:00 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19036849/demomysecondapp/Exercise2C.java
@@ -0,0 +1,32 @@
+package sg.edu.rp.c346.id19036849.demomysecondapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class Exercise2C extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise2_c);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
